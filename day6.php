1591ae000ad590b9365958e0c352b44c19364975
Aran Reeks <cdtreeks@gmail.com>
2017-12-06 08:40:37 +0000

    Completed day 6

--- day6.php
+++ day6.php
@@ -1,41 +1,23 @@
 <?php
-// WORK IN PROGRESS!!!
-
-//$input = '11 11 13 7 0 15 5 5 4 4 1 1 7 1 15 11';
-$input = '0 2 7 0';
+$input = '11 11 13 7 0 15 5 5 4 4 1 1 7 1 15 11';
 $blocks = explode(" ", $input);
 $num_blocks = count($blocks);
 $seen_patterns = [$blocks];
 $redistributions = 0;
 
-while (1) {
-    $key = get_highest_key($blocks);
-    $highest_block = $blocks[$key];
+while (true) {
+    $redistributions++;
+    $highest_key = array_keys($blocks, max($blocks))[0];
+    $highest_value = $blocks[$highest_key];
+    $blocks[$highest_key] = 0; // Set current to zero before redistribution
 
-    $blocks[$key] = 0;
-    for ($i = 0; $i < $highest_block; $i++) {
-        $current_key = $i + $key;
-        if ($current_key >= ($num_blocks - 1)) {
-            $current_key -= $num_blocks;
-        }
-
+    for ($i = 1; $i <= $highest_value; $i++) {
+        $current_key = ($i + $highest_key) % $num_blocks;
         $blocks[$current_key]++;
     }
-
-    $redistributions++;
-
-    echo 'Redistributions: ' . $redistributions . PHP_EOL;
-    echo 'Current: ' . implode("\t", $blocks) . PHP_EOL . PHP_EOL;
-
-    if (!in_array($blocks, $seen_patterns)) {
-        break;
-    }
+    if (in_array($blocks, $seen_patterns)) break;
     $seen_patterns[] = $blocks;
 }
 
-echo $redistributions;
-
-function get_highest_key($blocks) {
-    return array_keys($blocks, max($blocks))[0];
-}
-?>
+echo 'Part 1: ' . $redistributions . PHP_EOL;
+echo 'Part 2: ' . (count($seen_patterns) - array_keys($seen_patterns, $blocks)[0]);
